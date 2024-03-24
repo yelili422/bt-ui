3bccbcfebd53260ba019319e2cf04ee6b0f44a56
YeLi <i@yeli.io>
2024-03-24 23:41:54 +0800

    Modify vite proxy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const proxy_target =
+    mode === "development" ? "http://localhost:8081" : process.env.PROXY_TARGET;
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: proxy_target,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  plugins: [react()],
-})
+    plugins: [react()],
+  };
+});
